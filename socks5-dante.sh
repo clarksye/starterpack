70cc2fdb8f6df8f84f8569315a5394eae6f0761a
clarksye <clarksye@users.noreply.github.com>
2023-06-05 17:19:49 -0700

    Update socks5-dante.sh

--- socks5-dante.sh
+++ socks5-dante.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-# Step 2: Install Dante
-sudo apt update
-sudo apt install dante-server
+sudo apt-get update
+sudo apt-get install -y dante-server
 
-# Step 3: Configure Dante
-sudo tee /etc/sockd.conf > /dev/null << EOL
-internal: eth0 port = 1080
-external: eth0
-clientmethod: none
+sudo rm /etc/danted.conf
+sudo tee /etc/danted.conf > /dev/null << EOL
+logoutput: syslog
 user.privileged: root
-user.notprivileged: nobody
+user.unprivileged: nobody
+
+# The listening network interface or address.
+internal: 0.0.0.0 port=443
+
+# The proxying network interface or address.
+external: eth0
+
+# socks-rules determine what is proxied through the external interface.
+socksmethod: username
+
+# client-rules determine who can connect to the internal interface.
+clientmethod: none
+
 client pass {
     from: 0.0.0.0/0 to: 0.0.0.0/0
-    log: error
-    method: username
 }
+
+socks pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+}
+
 EOL
 
-# Step 4: Create User and Password
-sudo touch /etc/sockd.passwd
-sudo /usr/sbin/sockd -N -n -f /etc/sockd.conf -p /var/run/sockd.pid
-sudo sockdpasswd -c /etc/sockd.passwd clarksye
-# Ganti 'username' dengan nama pengguna yang Anda inginkan
-# Anda akan diminta untuk memasukkan dan mengonfirmasi kata sandi
+sudo useradd -r -s /bin/false clarksye
+echo -e "user123\nuser123" | sudo passwd clarksye
 
-sudo pkill sockd
 
-# Step 5: Restart Dante
-sudo systemctl restart danted
-
-echo "Dante SOCKS5 server setup completed."
+sudo systemctl restart danted.service
